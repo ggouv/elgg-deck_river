ee195bbe21960268da47baacc5c15b9bc313eb0d
ManUtopiK <emmanuel.salomon@free.fr>
2011-10-29 22:42:08 -0300

    adding french language

---
+++ languages/fr.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Deck-river French language file.
+ *
+ */
+
+$french = array(
+	'deck_river:edit' => 'Éditer les paramètres de la colonne',
+	'deck_river:refresh' => 'Rafraîchir la colonne',
+	'deck_river:limitColumnReached' => 'Le nombre maximum de colonne est atteint.',
+	'deck_river:settings' => 'Paramètres de la colonne %s',
+	'deck_river:type' => 'Type',
+);
+
+add_translation('fr', $french);
