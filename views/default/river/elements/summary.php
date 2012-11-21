cf6449d1e266d994baa64323d3e043af139a3d33
ManUtopiK <emmanuel.salomon@free.fr>
2012-11-21 01:52:50 +0100

    Perform elgg_echo river:ingroup in river item

--- views/default/river/elements/summary.php
+++ views/default/river/elements/summary.php
@@ -39,14 +39,16 @@ if ($container instanceof ElggGroup && $container->guid != elgg_get_page_owner_g
 		'is_trusted' => true,
 	);
 	$group_link = elgg_view('output/url', $params);
-	$group_string = elgg_echo('river:ingroup', array($group_link));
+	
+	$key = "river:ingroup:$action:$type:$subtype";
+	$group_string = elgg_echo($key, array($group_link));
+	if ($group_string == $key) $group_string = elgg_echo('river:ingroup', array($group_link));
 }
 
 // check summary translation keys.
 // will use the $type:$subtype if that's defined, otherwise just uses $type:default
 $key = "river:$action:$type:$subtype";
 $summary = elgg_echo($key, array($subject_link, $object_link));
-
 if ($summary == $key) {
 	$key = "river:$action:$type:default";
 	$summary = elgg_echo($key, array($subject_link, $object_link));
