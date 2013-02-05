4f253453c77691e0401c0dcdf003dbef35f43e46
ManUtopiK <emmanuel.salomon@free.fr>
2013-02-05 09:52:19 +0100

    Fix owner view breadcrumb

--- pages/thewire/owner.php
+++ pages/thewire/owner.php
@@ -6,24 +6,20 @@
 
 $owner = elgg_get_page_owner_entity();
 if (!$owner) {
-	forward('thewire/all');
+	forward(REFERER);
 }
+
+elgg_push_breadcrumb($owner->name, 'profile/' . $owner->name);
+elgg_push_breadcrumb(elgg_echo('thewire:breadcrumb:user'));
 
 $title = elgg_echo('thewire:user', array($owner->name));
 
-elgg_push_breadcrumb(elgg_echo('thewire'), "thewire/all");
-elgg_push_breadcrumb($owner->name);
-
-$context = '';
-if (elgg_get_logged_in_user_guid() == $owner->guid) {
-	$context = 'mine';
-}
 
 $content = '<div id="column"><ul class="elgg-river elgg-list">' . elgg_view('graphics/ajax_loader', array('hidden' => false)) . '</ul></div>';
 $content .= "<input id=\"json-river-owner\" class=\"hidden\" value=\"{$owner->guid}\">";
 
 $body = elgg_view_layout('content', array(
-	'filter_context' => $context,
+	'filter_override' => '',
 	'content' => $content,
 	'title' => $title,
 	'sidebar' => elgg_view('thewire/sidebar'),
