75b2bed47c9890ed0347a6a8159c893237d7d531
ManUtopiK <emmanuel.salomon@free.fr>
2013-06-26 10:51:20 +0200

    Fix comment for workflow_card and markdown_wiki page

--- views/json/river/annotation/generic_comment/create.php
+++ views/json/river/annotation/generic_comment/create.php
@@ -9,10 +9,28 @@ global $jsonexport;
 
 $comment = $vars['item']->getAnnotation();
 
-$vars['item']->summary = elgg_view('river/elements/summary', array(
-																'item' => $vars['item'],
-																'hash' => '#item-annotation-' . $comment->id
-								), FALSE, FALSE, 'default');
-$vars['item']->message = deck_river_wire_filter(elgg_get_excerpt($comment->value), 140);
+switch ($vars['item']->subtype) {
+	case 'markdown_wiki':
+		$vars['item']->summary = elgg_view('river/elements/markdown_wiki_comment_summary', array(
+				'item' => $vars['item'],
+				'hash' => '#item-annotation-' . $comment->id
+		), FALSE, FALSE, 'default');
+		break;
+	case 'workflow_card':
+		$vars['item']->summary = elgg_view('river/elements/workflow_card_comment_summary', array(
+				'item' => $vars['item'],
+				'hash' => '#item-annotation-' . $comment->id
+		), FALSE, FALSE, 'default');
+		break;
+	default:
+		$vars['item']->summary = elgg_view('river/elements/summary', array(
+				'item' => $vars['item'],
+				'hash' => '#item-annotation-' . $comment->id
+		), FALSE, FALSE, 'default');
+		break;
+}
+
+
+$vars['item']->message = deck_river_wire_filter(elgg_get_excerpt($comment->value, 140));
 
 $jsonexport['activity'][] = $vars['item'];
